62c4cb77f625c771763fdf1beda4cb6847b10f38
Alice Wood <alicesophiewood@gmail.com>
2023-05-25 16:26:45 +0100

    thermostat set up and constructs with correct starting temp

---
+++ thermostat/thermostat.test.js
@@ -0,0 +1,8 @@
+const Thermostat = require("./Thermostat");
+
+describe("Thermostat", () => {
+  it("returns the initial temperature of 20", () => {
+    const thermostat = new Thermostat();
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+});
