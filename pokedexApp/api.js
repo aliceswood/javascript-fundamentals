fdd38af0192fc8e32e3378a4784166936fa53ed2
Alice Wood <alicesophiewood@gmail.com>
2023-05-26 13:38:42 +0100

    async and await tests and methods refactored

--- pokedexApp/api.js
+++ pokedexApp/api.js
@@ -3,35 +3,32 @@ class Pokedex {
     this.pokedex = [];
   }
 
-  fetchPokemon(pokemonName) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then((response) => response.json())
-
-      .then((data) => {
-        let pokemonDetails = {
-          name: data.name,
-          id: data.id,
-          height: data.height,
-          weight: data.weight,
-          types: data.types.map((pokemon) => {
-            return pokemon.type.name;
-          }),
-        };
-        return pokemonDetails;
-      });
+  async fetchPokemon(pokemonName) {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const data = await response.json();
+    let pokemonDetails = {
+      name: data.name,
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+      types: data.types.map((pokemon) => {
+        return pokemon.type.name;
+      }),
+    };
+    return pokemonDetails;
   }
 
-  catch(pokemon) {
-    const pokemonToCatch = this.fetchPokemon(pokemon)
-      pokemonToCatch.then((pokemon) => {
-       return this.pokedex.push(pokemon); 
-      });
-      return pokemonToCatch
+  async catch(pokemon) {
+    const pokemonToCatch = await this.fetchPokemon(pokemon);
+    this.pokedex.push(pokemonToCatch);
+    return pokemonToCatch;
   }
 
   all() {
-    return this.pokedex.map((pokemon) =>{
-      return pokemon.name
+    return this.pokedex.map((pokemon) => {
+      return pokemon.name;
     });
   }
 }
--- pokedexApp/api.test.js
+++ pokedexApp/api.test.js
@@ -3,11 +3,10 @@ const Pokedex = require("./api");
 describe("Pokedex", () => {
   it("returns needed pokemon details with done param", (done) => {
     const pokedex = new Pokedex();
-    pokedex.fetchPokemon("charizard")
-      .then((pokemon) => {
-        expect(pokemon.id).toEqual(6);
-        done();
-      });
+    pokedex.fetchPokemon("charizard").then((pokemon) => {
+      expect(pokemon.id).toEqual(6);
+      done();
+    });
   });
 
   it("returns the promise of pokemon details", () => {
@@ -17,26 +16,38 @@ describe("Pokedex", () => {
     });
   });
 
+  it("uses async and await to fetch pokemon details", async () => {
+    const pokedex = new Pokedex();
+    const pokemon = await pokedex.fetchPokemon("charizard");
+    expect(pokemon.id).toEqual(6);
+  });
+
   it("catches a given pokemon", (done) => {
     const pokedex = new Pokedex();
-    pokedex.catch("pikachu")
-      .then((pokemon) => {
-        expect(pokedex.all()).toEqual(["pikachu"]);
-        done();
-      });
+    pokedex.catch("pikachu").then((pokemon) => {
+      expect(pokedex.all()).toEqual(["pikachu"]);
+      done();
+    });
   });
 
   it("catches multiple pokemon", (done) => {
     const pokedex = new Pokedex();
-    pokedex.catch("pikachu")
-      .then((pokemon) => {
-        expect(pokedex.all()).toEqual(["pikachu"]);
-        done();
-      });
-    pokedex.catch("jigglypuff")
-      .then((pokemon) => {
-        expect(pokedex.all()).toEqual(["pikachu", "jigglypuff"]);
-        done();
-      });
+    pokedex.catch("pikachu").then((pokemon) => {
+      expect(pokedex.all()).toEqual(["pikachu"]);
+      done();
+    });
+    pokedex.catch("jigglypuff").then((pokemon) => {
+      expect(pokedex.all()).toEqual(["pikachu", "jigglypuff"]);
+      done();
+    });
+  });
+
+  it("catches multiple pokemon using async and await", async () => {
+    const pokedex = new Pokedex();
+    const pokemon1 = await pokedex.catch("pikachu");
+    expect(pokedex.all()).toEqual([pokemon1.name]);
+
+    const pokemon2 = await pokedex.catch("jigglypuff");
+    expect(pokedex.all()).toEqual([pokemon1.name, pokemon2.name]);
   });
 });
