4b94e0a58cb6891d1f683887e284aaa9589d668f
Alice Wood <alicesophiewood@gmail.com>
2023-05-26 11:27:03 +0100

    fetching and returning pokemon data from API

--- pokedexApp/api.js
+++ pokedexApp/api.js
@@ -2,7 +2,19 @@
   const fetchPokemon = (pokemonName) => {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      // .then((data) => console.log(data));
+      .then((data)=> {
+        let pokemonDetails = {
+          name: data.name,
+          id: data.id,
+          height: data.height,
+          weight: data.weight,
+          types: data.types.map((pokemon) => {
+            return pokemon.type.name
+          })
+        }
+        return console.log(pokemonDetails);
+      });
   };
 
 
