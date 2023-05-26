f1314bb93de629443c102398ef6d94fa39926793
Alice Wood <alicesophiewood@gmail.com>
2023-05-26 10:55:51 +0100

    Pokedex app started with fetch promise returning

---
+++ pokedexApp/api.js
@@ -0,0 +1,10 @@
+
+  const fetchPokemon = (pokemonName) => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+
+
+module.exports = fetchPokemon;
