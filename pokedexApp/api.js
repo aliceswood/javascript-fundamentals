5ceb302eb9186971d7e996c195ef027d4c786d5b
Alice Wood <alicesophiewood@gmail.com>
2023-05-26 13:12:16 +0100

    tests added for the catch and all functions and tests catching single and multiple pokemon

--- pokedexApp/api.js
+++ pokedexApp/api.js
@@ -1,25 +1,38 @@
-class Pokedex{
+class Pokedex {
   constructor() {
-    this.pokedex = []
+    this.pokedex = [];
   }
 
   fetchPokemon(pokemonName) {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then((response) => response.json())
-     
-      .then((data)=> {
+
+      .then((data) => {
         let pokemonDetails = {
           name: data.name,
           id: data.id,
           height: data.height,
           weight: data.weight,
           types: data.types.map((pokemon) => {
-            return pokemon.type.name
-          })
-        }
+            return pokemon.type.name;
+          }),
+        };
         return pokemonDetails;
       });
-  };
+  }
 
+  catch(pokemon) {
+    const pokemonToCatch = this.fetchPokemon(pokemon)
+      pokemonToCatch.then((pokemon) => {
+       return this.pokedex.push(pokemon); 
+      });
+      return pokemonToCatch
+  }
+
+  all() {
+    return this.pokedex.map((pokemon) =>{
+      return pokemon.name
+    });
+  }
 }
 module.exports = Pokedex;
--- pokedexApp/api.test.js
+++ pokedexApp/api.test.js
@@ -1,22 +1,42 @@
-const Pokedex = require('./api')
-const pokedex = new Pokedex()
+const Pokedex = require("./api");
 
-describe('Pokedex', () => {
-  it('returns needed pokemon details with done param', (done) => {
+describe("Pokedex", () => {
+  it("returns needed pokemon details with done param", (done) => {
     const pokedex = new Pokedex();
-    pokedex.fetchPokemon('charizard')
-    .then((pokemon) => {
-      expect(pokemon.id).toEqual(6);
-      done();
-    });
-  })
+    pokedex.fetchPokemon("charizard")
+      .then((pokemon) => {
+        expect(pokemon.id).toEqual(6);
+        done();
+      });
+  });
 
-  it('returns the promise of pokemon details', () => {
+  it("returns the promise of pokemon details", () => {
     const pokedex = new Pokedex();
-    pokedex.fetchPokemon('charizard')
-    .then((pokemon) => {
+    pokedex.fetchPokemon("charizard").then((pokemon) => {
       expect(pokemon.id).toEqual(6);
     });
   });
 
-})
+  it("catches a given pokemon", (done) => {
+    const pokedex = new Pokedex();
+    pokedex.catch("pikachu")
+      .then((pokemon) => {
+        expect(pokedex.all()).toEqual(["pikachu"]);
+        done();
+      });
+  });
+
+  it("catches multiple pokemon", (done) => {
+    const pokedex = new Pokedex();
+    pokedex.catch("pikachu")
+      .then((pokemon) => {
+        expect(pokedex.all()).toEqual(["pikachu"]);
+        done();
+      });
+    pokedex.catch("jigglypuff")
+      .then((pokemon) => {
+        expect(pokedex.all()).toEqual(["pikachu", "jigglypuff"]);
+        done();
+      });
+  });
+});
